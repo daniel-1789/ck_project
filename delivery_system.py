ee7fd24e6c8d6067ca206759748acc5d9899d7af
Daniel Stack <dstack1776@gmail.com>
2020-04-18 19:17:38 -0400

    further cleanup

--- delivery_system.py
+++ delivery_system.py
@@ -4,10 +4,14 @@ import random
 import sys
 from enum import Enum
 
-curr_tick = 0
-shelves = []
+curr_tick = 0  # global clock
+
 
 class FoodItem:
+    """
+    Class that represents an item of food either on the shelf or being serviced by courier
+    """
+
     def __init__(self, food_dict):
         self.id = food_dict['id']
         self.name = food_dict['name']
@@ -18,12 +22,23 @@ class FoodItem:
         self.value = 1.0
 
     def increase_time_on_shelf(self, time_inc, shelf_mult=1):
+        """
+        Increase the amount of time food is on the shef
+        :param time_inc: How many ticks to increase by
+        :param shelf_mult: multiplier to increase decay rate - i.e. if on overflow shelf
+        :return: value of this food item after it has been decreased due to passage of time
+        """
         self.time_on_shelf += time_inc
-        self.value = (float(self.shelf_life) - (float(self.decay_rate) * self.time_on_shelf * shelf_mult))/float(self.shelf_life)
+        self.value = (float(self.shelf_life) - (float(self.decay_rate) * self.time_on_shelf * shelf_mult)) / float(
+            self.shelf_life)
         return self.value
 
 
 class FoodShelf:
+    """
+    class to represent a shelf of food awaiting pickup
+    """
+
     def __init__(self, shelf_type, shelf_capacity):
         self.shelf_type = shelf_type
         self.shelf_max = shelf_capacity
@@ -31,19 +46,30 @@ class FoodShelf:
         self.decay_mult = 1.0 if shelf_type != 'overflow' else 2.0
 
     def find_cheapest_item(self):
+        """
+        Simple linear search to find cheapest item - used when deciding what to toss out/move to a new shelf.
+        Definitely opportunity for optimization should the shelf change from room for double-digits of entries
+        to thousands/millions/etc.
+        :return: cheapest item's id
+        """
         # linear search of the shelf for cheapest item
         if len(self.food_dict) == 0:
             return None
         else:
             cheapest_value = float('inf')
             cheapest_key = None
-            for k,v in self.food_dict.items():
+            for k, v in self.food_dict.items():
                 if v.value < cheapest_value:
                     cheapest_key = k
                     cheapest_value = v.value
         return cheapest_key
 
     def add_food_to_shelf(self, food_item: FoodItem):
+        """
+        Add a food item to this shelf. If the shelf is full bump the least valuable item
+        :param food_item: food item to add
+        :return: None if food added to shelf without bumping anything, otherwise the food that was bumped
+        """
         removed_food = None
         if len(self.food_dict) >= self.shelf_max:
             # remove the minimum value item - just a linear search for now
@@ -55,10 +81,20 @@ class FoodShelf:
         return removed_food
 
     def remove_food_from_shelf(self, food_key):
+        """
+        Remove food with the given id from the shelf
+        :param food_key: id of food to be removed
+        :return: The FoodItem removed, None if it could not be found
+        """
         removed_food = self.food_dict.pop(food_key, None)
         return removed_food
 
     def add_ticks(self, ticks_to_add):
+        """
+        Add ticks to all the food on the shelf, if anything goes bad/spoils, remove it
+        :param ticks_to_add:
+        :return: nothing
+        """
         food_to_remove = []
         for curr_food in self.food_dict.values():
             curr_value = curr_food.increase_time_on_shelf(ticks_to_add, shelf_mult=self.decay_mult)
@@ -69,14 +105,16 @@ class FoodShelf:
             log_action({self.shelf_type: self}, 'Threw out expired {}'.format(curr_food))
 
 
-def do_tick_housekeeping(time_between_ticks):
-    time.sleep(time_between_ticks)
-
 def restore_to_proper_shelf(shelves):
-    # walk through the overflow shelf and see if items can be put back where they belong. remember don't remove
-    # from what we are iterating over
+    """
+    At the start of every tick, see if the items on the standby shelf can find their way back to the
+    proper shelves. Could add some complexity here at a later point if desired to optimize what gets moved
+    and to speed things up but with shelves having room in the double digits not needed at this point.
+    :param shelves: Dictionary of shelves - hot, cold, frozen, standby.
+    :return: Nothing
+    """
     restored_items = []
-    for k,v in shelves['overflow'].food_dict.items():
+    for k, v in shelves['overflow'].food_dict.items():
         proper_shelf = shelves[v.temp]
         if len(proper_shelf.food_dict) < proper_shelf.shelf_max:
             restored_items.append(k)
@@ -85,35 +123,63 @@ def restore_to_proper_shelf(shelves):
         shelves['overflow'].food_dict.pop(curr_key)
     log_action(shelves, 'shelf restore')
 
+
 class NewItemStatus(Enum):
     ok = 0
     no_shelf = 1
-    moved_to_overflow = 2
-    moved_from_overflow = 3
+    moved_item_to_overflow = 2
+    removed_item_for_room = 3
+    already_shelved = 4
+
 
 def process_new_item(shelves, curr_item: FoodItem):
-    rc = 'ok'
-    # Step 1 - see if can fit into proper shelf without dropping anything
+    """
+    Add a new item to the proper shelf, rearrange other items as required
+    :param shelves: Dictionary of shelves
+    :param curr_item: Item to be added
+    :return: NewItemStatus - unless the shelf cannot be found (no_shelf) or the item is already in a shelf
+      (already_shelved), item was added. Possible warnings/ informationals include if
+      other items had to be moved to overflow - and possibly if something else was moved from it and
+      tossed out
+    """
+    rc = NewItemStatus.ok
+
+    # Step 1 - Make sure it does not already exist
+    for curr_shelf in shelves.values():
+        if curr_shelf.food_dict.get(curr_item.id, None) is not None:
+            log_action(shelves,
+                       'New item {} already on shelf {}.'.format(
+                           curr_item.id, curr_shelf.shelf_type
+                       ))
+
+            return NewItemStatus.already_shelved
+
+    # Step 2 - see if can fit into proper shelf without dropping anything
     proper_shelf = shelves.get(curr_item.temp, None)
     if proper_shelf is None:
+        log_action(shelves,
+                   'New item {} cannot be added to non-existent shelf {}.'.format(
+                       curr_item.id, curr_item.temp
+                   ))
+
         return NewItemStatus.no_shelf
 
     dropped_item = proper_shelf.add_food_to_shelf(curr_item)
 
-    # step 2 - if we removed something add it to overflow
+    # Step 3 - if we removed something add it to overflow
     if dropped_item is not None:
         lost_item = shelves['overflow'].add_food_to_shelf(dropped_item)
         if lost_item is not None:
             log_action(shelves,
                        'New item {} added. Moved {} to overflow. Tossed {}'.format(
                            curr_item.id, dropped_item.id, lost_item.id))
-            rc = NewItemStatus.moved_from_overflow
+            rc = NewItemStatus.removed_item_for_room
         else:
             log_action(shelves,
                        'New item {} added. Moved {} to overflow.'.format(
                            curr_item.id, dropped_item.id
                        ))
-            rc = NewItemStatus.moved_to_overflow
+            rc = NewItemStatus.moved_item_to_overflow
     else:
         log_action(shelves,
                    'New item {} added.'.format(
@@ -124,6 +190,14 @@ def process_new_item(shelves, curr_item: FoodItem):
 
 
 def create_shelves(hot_size, cold_size, frozen_size, overflow_size):
+    """
+    Simply create the shelves of the indicated sizes
+    :param hot_size:
+    :param cold_size:
+    :param frozen_size:
+    :param overflow_size:
+    :return:
+    """
     hot_shelf = FoodShelf('hot', hot_size)
     cold_shelf = FoodShelf('cold', cold_size)
     frozen_shelf = FoodShelf('frozen', frozen_size)
@@ -131,9 +205,17 @@ def create_shelves(hot_size, cold_size, frozen_size, overflow_size):
     shelves = {'hot': hot_shelf, 'cold': cold_shelf, 'frozen': frozen_shelf, 'overflow': overflow_shelf}
     return shelves
 
+
 def courier_action(shelves, couriers):
+    """
+    Look in the dictionary of courier times to see if anything needs to be retrieved in this tick
+    :param shelves:
+    :param couriers:dictionary of couriers - key is the tick they arrive, value is a list of FoodItems to pick up
+    :return: List of FoodItems removed from the shelves by the courier
+    """
     global curr_tick
-    removed_list = []
+
+    removed_list = []  # list of what is removed from all shelves by the courier arrival
 
     curr_couriers = couriers.pop(curr_tick, [])
     for courier in curr_couriers:
@@ -151,7 +233,15 @@ def courier_action(shelves, couriers):
             removed_list.append(removed_food)
     return removed_list
 
+
 def log_action(shelves, action_string):
+    """
+    Simple logger. Could redirect this to a standard logging file if desired but for this exervcise seemed to
+    be easiest just to print
+    :param shelves:
+    :param action_string:
+    :return:
+    """
     global curr_tick
     print('At time tick {}, {}'.format(curr_tick, action_string))
     if shelves is not None:
@@ -164,11 +254,28 @@ def log_action(shelves, action_string):
                     curr_item.id, curr_item.name, curr_item.temp, curr_item.time_on_shelf, curr_item.value)
                 )
             print('---')
-    print ('==========================================')
+    print('==========================================')
 
 
-
-def tick_thread( time_between_ticks, number_per_tick, json_fn, hot_size, cold_size, frozen_size, overflow_size, courier_low, courier_size):
+def main(time_between_ticks, number_per_tick, json_fn, hot_size, cold_size, frozen_size, overflow_size, courier_low,
+         courier_size):
+    """
+    main routine - takes the parameters parsed from the command line and loops until both the json has been
+    completely ingested and all scheduled couriers are done. A courier is created immediately after an item
+    is put on a shelf. It is scheduled for a random time in the future. A dictionary of ticks holds lists of
+    couriers (just another instance of the FoodItem to be grabbed) - doing it as dictionary allows for quick
+    hashing.
+    :param time_between_ticks:
+    :param number_per_tick:
+    :param json_fn:
+    :param hot_size:
+    :param cold_size:
+    :param frozen_size:
+    :param overflow_size:
+    :param courier_low:
+    :param courier_size:
+    :return: nothing
+    """
     global curr_tick
 
     total_ingested = 0
@@ -178,10 +285,10 @@ def tick_thread( time_between_ticks, number_per_tick, json_fn, hot_size, cold_si
         json_list = json.load(target)
     len_list = len(json_list)
     jsons = iter(json_list)
-    couriers = dict()
+    couriers = dict()  # whenever we schedule a courier, create/update dictionary entry with their arrival time
 
-    while total_ingested < len_list or len(couriers) > 0 :
-        do_tick_housekeeping(time_between_ticks)
+    while total_ingested < len_list or len(couriers) > 0:
+        time.sleep(time_between_ticks)
         curr_tick += 1
         for curr_shelf in shelves.values():
             curr_shelf.add_ticks(1)
@@ -192,17 +299,37 @@ def tick_thread( time_between_ticks, number_per_tick, json_fn, hot_size, cold_si
                 curr_item = next(jsons)
                 food_item = FoodItem(curr_item)
                 total_ingested += 1
-                process_new_item(shelves, food_item)
-                courier_time = random.randint(courier_low,courier_low+courier_size) + curr_tick
-                courier_slot = couriers.get(courier_time, [])
-                courier_slot.append(food_item)
-                couriers[courier_time] = courier_slot
+                rc = process_new_item(shelves, food_item)
+                if rc != NewItemStatus.no_shelf and rc != NewItemStatus.already_shelved:
+                    courier_time = random.randint(courier_low, courier_low + courier_size) + curr_tick
+                    courier_slot = couriers.pop(courier_time, [])
+                    courier_slot.append(food_item)
+                    couriers[courier_time] = courier_slot
                 if total_ingested >= len_list:
                     break
 
-if __name__ == "__main__":
 
-    print (sys.argv)
+def usage_message():
+    """
+    Simple usage message.
+    :return: Exits 1
+    """
+    print('Usage - all parameters are optional')
+    print('--tick <float> time_in_sec_between_ticks')
+    print('--num_ingest number_orders_to_ingest_per_tick')
+    print('--input json_file_name')
+    print('--num_hot hot_shelf_capacity')
+    print('--num_cold cold_shelf_capacity')
+    print('--num_frozen frozen_shelf_capacity')
+    print('--num_overflow overflow_shelf_capacity')
+    print('--courier_low min_seconds_delay_for_courier')
+    print('--courier_num num_additional_possible_seconds_delay')
+    print('--help')
+    exit(1)
+
+
+if __name__ == "__main__":
+    # setup default values
     fn = 'orders.json'
     tick_time = 1.0
     num_ingest = 2
@@ -213,39 +340,45 @@ if __name__ == "__main__":
     courier_low = 2
     courier_num = 5
 
-    if len(sys.argv) > 1:
-        pointer = 1
-        while pointer < len(sys.argv):
-            curr_arg = sys.argv[pointer]
-            print(curr_arg)
-            pointer += 1
-            if curr_arg == '--tick':
-                tick_time = float(sys.argv[pointer])
+    try:
+        if len(sys.argv) > 1:
+            pointer = 1
+            while pointer < len(sys.argv):
+                curr_arg = sys.argv[pointer]
+                print(curr_arg)
                 pointer += 1
-            elif curr_arg == '--num_ingest':
-                num_ingest = int(sys.argv[pointer])
-                pointer += 1
-            elif curr_arg == '--input':
-                fn = sys.argv[pointer]
-                pointer += 1
-            elif curr_arg == '--num_hot':
-                hot_size = int(sys.argv[pointer])
-                pointer += 1
-            elif curr_arg == '--num_cold':
-                cold_size = int(sys.argv[pointer])
-                pointer += 1
-            elif curr_arg == '--num_frozen':
-                frozen_size = int(sys.argv[pointer])
-                pointer += 1
-            elif curr_arg == '--num_overflow':
-                overflow_size = int(sys.argv[pointer])
-                pointer += 1
-            elif curr_arg == '--courier_low':
-                courier_low = int(sys.argv[pointer])
-                pointer += 1
-            elif curr_arg == '--courier_num':
-                courier_num = int(sys.argv[pointer])
-                pointer += 1
+                if curr_arg == '--tick':
+                    tick_time = float(sys.argv[pointer])
+                    pointer += 1
+                elif curr_arg == '--num_ingest':
+                    num_ingest = int(sys.argv[pointer])
+                    pointer += 1
+                elif curr_arg == '--input':
+                    fn = sys.argv[pointer]
+                    pointer += 1
+                elif curr_arg == '--num_hot':
+                    hot_size = int(sys.argv[pointer])
+                    pointer += 1
+                elif curr_arg == '--num_cold':
+                    cold_size = int(sys.argv[pointer])
+                    pointer += 1
+                elif curr_arg == '--num_frozen':
+                    frozen_size = int(sys.argv[pointer])
+                    pointer += 1
+                elif curr_arg == '--num_overflow':
+                    overflow_size = int(sys.argv[pointer])
+                    pointer += 1
+                elif curr_arg == '--courier_low':
+                    courier_low = int(sys.argv[pointer])
+                    pointer += 1
+                elif curr_arg == '--courier_num':
+                    courier_num = int(sys.argv[pointer])
+                    pointer += 1
+                elif curr_arg == '--help':
+                    raise ()
+                else:
+                    raise ()
+    except:
+        usage_message()
 
-    tick_thread(tick_time, num_ingest,fn, hot_size, cold_size, frozen_size, overflow_size, courier_low, courier_num)
-
+    main(tick_time, num_ingest, fn, hot_size, cold_size, frozen_size, overflow_size, courier_low, courier_num)
--- delivery_system_test.py
+++ delivery_system_test.py
@@ -1,11 +1,19 @@
 import unittest
-import delivery_system
-from delivery_system import shelves, curr_tick, create_shelves, FoodItem, process_new_item, NewItemStatus, courier_action
+from delivery_system import create_shelves, FoodItem, process_new_item, NewItemStatus, courier_action, restore_to_proper_shelf
 import json
 
+shelves = dict()
+curr_tick = 0
+
 class MyTestCase(unittest.TestCase):
-    def build_full_dicts(self):
+    def setUp(self):
+        """
+        Creates the global shelves - reads the default json file in and builds it - the idea is to
+        make all the shelves full. We can then adjust them as required in individual tests.
+        :return:
+        """
         global shelves
+        shelves = dict()
         shelves = create_shelves(10, 10, 10, 15)
         with open('orders.json', 'r') as target:
             json_list = json.load(target)
@@ -18,6 +26,10 @@ class MyTestCase(unittest.TestCase):
             process_new_item(shelves, food_item)
 
     def create_a_food_item(self):
+        """
+        Creates a FoodItem to later allow a new order being put on shelf
+        :return:
+        """
         food_dict = dict()
         food_dict['id'] = 'unit-test'
         food_dict['name'] = 'Unit Test Sundae'
@@ -28,33 +40,63 @@ class MyTestCase(unittest.TestCase):
         return food_item
 
     def test_add_all_full(self):
+        """
+        Attempt to add an item to the frozen shelf - should work, but should kick off an item
+        from the frozen shelf which cannot get put on overflow as that is full too
+        :return:
+        """
         global shelves
-        self.build_full_dicts()
         food_item = self.create_a_food_item()
         rc = process_new_item(shelves, food_item)
-        self.assertEqual(rc, NewItemStatus.moved_from_overflow)
+        self.assertEqual(rc, NewItemStatus.removed_item_for_room)
+
+    def test_add_already_present(self):
+        """
+        Test that code can detect an attempt to add an item to a shelf when item is already on a shelf
+        :return:
+        """
+        global shelves
+        food_item = self.create_a_food_item()
+        # remove an entry from the frozen
+        shelves['frozen'].food_dict.popitem()
+        rc = process_new_item(shelves, food_item)
+        self.assertEqual(rc, NewItemStatus.ok)
+        food_item_dup = self.create_a_food_item()
+        food_item_dup.temp = 'hot'
+        rc = process_new_item(shelves, food_item_dup)
+        self.assertEqual(rc, NewItemStatus.already_shelved)
 
     def test_add_dest_full(self):
+        """
+        Destination shelf is full. Overflow shelf has room. Make sure goes into right shelf and
+        bump to overflow
+        :return:
+        """
         global shelves
-        self.build_full_dicts()
         # remove an entry from the overflow
         shelves['overflow'].food_dict.popitem()
         food_item = self.create_a_food_item()
         rc = process_new_item(shelves, food_item)
-        self.assertEqual(rc, NewItemStatus.moved_to_overflow)
+        self.assertEqual(rc, NewItemStatus.moved_item_to_overflow)
 
     def test_add_dest_has_room(self):
+        """
+        Destination shelf has room. Nothing needs to go to overflow or be dropped.
+        :return:
+        """
         global shelves
-        self.build_full_dicts()
-        # remove an entry from the overflow
+        # remove an entry from the frozen
         shelves['frozen'].food_dict.popitem()
         food_item = self.create_a_food_item()
         rc = process_new_item(shelves, food_item)
         self.assertEqual(rc, NewItemStatus.ok)
 
     def test_add_no_matching_dict(self):
+        """
+        Attempt to add to a non-existent shelf
+        :return:
+        """
         global shelves
-        self.build_full_dicts()
         # remove an entry from the overflow
         food_item = self.create_a_food_item()
         food_item.temp = 'absolute zero'
@@ -62,6 +104,12 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(rc, NewItemStatus.no_shelf)
 
     def setup_couriers(self, tick, food_item):
+        """
+        Create a mini-courier dict
+        :param tick: current time in ticks
+        :param food_item: food_item to add to mini-courier
+        :return: courier dictionary with just one entry
+        """
         couriers = dict()
         courier_time =  tick
         courier_slot = couriers.get(courier_time, [])
@@ -70,42 +118,57 @@ class MyTestCase(unittest.TestCase):
         return couriers
 
     def test_courier_get_from_overflow_shelf(self):
+        """
+        verify the courier gets an entry that is on overflow
+        :return:
+        """
         global shelves
         global curr_tick
-        self.build_full_dicts()
         # remove an entry from the overflow
         shelves['overflow'].food_dict.popitem()
         food_item = self.create_a_food_item()
-        rc = process_new_item(shelves, food_item)
-        self.assertEqual(rc, NewItemStatus.moved_to_overflow)
+        shelves['overflow'].food_dict = dict()  # destroy the old dict
+        shelves['overflow'].food_dict[food_item.id] = food_item  # force it on the overflow
+        now_on_overflow = shelves['overflow'].food_dict.get(food_item.id, None) is not None
+        self.assertEqual(now_on_overflow, True)
 
         couriers = self.setup_couriers(curr_tick, food_item)
         retrieved_items = courier_action(shelves, couriers)
-        pass
         removed_it = food_item in retrieved_items
         self.assertEqual(True, removed_it)
+        now_on_overflow = shelves['overflow'].food_dict.get(food_item.id, None) is not None
+        self.assertEqual(now_on_overflow, False)
 
     def test_courier_get_from_right_shelf(self):
+        """
+        verify the courier gets an entry that is on the proper shelf
+        :return:
+        """
         global shelves
         global curr_tick
-        self.build_full_dicts()
         # remove an entry from the overflow
         shelves['frozen'].food_dict.popitem()
         food_item = self.create_a_food_item()
         rc = process_new_item(shelves, food_item)
         self.assertEqual(rc, NewItemStatus.ok)
+        now_on_frozen = shelves['frozen'].food_dict.get(food_item.id, None) is not None
+        self.assertEqual(now_on_frozen, True)
 
         couriers = self.setup_couriers(curr_tick, food_item)
         retrieved_items = courier_action(shelves, couriers)
-        pass
         removed_it = food_item in retrieved_items
         self.assertEqual(True, removed_it)
+        now_on_frozen = shelves['frozen'].food_dict.get(food_item.id, None) is not None
+        self.assertEqual(now_on_frozen, False)
 
     def test_courier_item_not_present(self):
+        """
+        Verify courier fails at getting a non-existent item
+        :return:
+        """
         global shelves
         global curr_tick
-        self.build_full_dicts()
-        food_item = self.create_a_food_item()
+        food_item = self.create_a_food_item()  # do not insert this one onto a shelf
 
         couriers = self.setup_couriers(curr_tick, food_item)
         retrieved_items = courier_action(shelves, couriers)
@@ -113,9 +176,12 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(False, removed_it)
 
     def test_food_spoil(self):
+        """
+        Force food to spoil, detect it is removed
+        :return:
+        """
         global shelves
         global curr_tick
-        self.build_full_dicts()
         # remove an entry from the frozen
         shelves['frozen'].food_dict.popitem()
         food_item = self.create_a_food_item()
@@ -129,13 +195,15 @@ class MyTestCase(unittest.TestCase):
 
         shelves['frozen'].add_ticks(1)
         still_present = shelves['frozen'].food_dict.get(food_item.id, None) is not None
-        pass
         self.assertEqual(False, still_present)
 
     def test_food_spoil_overflow_faster(self):
+        """
+        Verify the overflow shelf spoils faster
+        :return:
+        """
         global shelves
         global curr_tick
-        self.build_full_dicts()
         # remove an entry from the overflow
         shelves['overflow'].food_dict.popitem()
         food_item = self.create_a_food_item()
@@ -149,9 +217,12 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(False, still_present)
 
     def test_cheapest(self):
+        """
+        Test that the cheapest item can be found
+        :return:
+        """
         global shelves
         global curr_tick
-        self.build_full_dicts()
         # remove an entry from the frozen
         shelves['frozen'].food_dict.popitem()
         food_item = self.create_a_food_item()
@@ -160,6 +231,33 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(rc, NewItemStatus.ok)
         cheapest_id = shelves['frozen'].find_cheapest_item()
         self.assertEqual(cheapest_id, food_item.id)
+
+    def test_restore(self):
+        """
+        Test that rebalancing will move things to shelf if there is room
+        :return:
+        """
+        global shelves
+        global curr_tick
+
+        food_item = self.create_a_food_item()
+
+        shelves['overflow'].food_dict = dict()  # destroy the old dict
+        shelves['overflow'].food_dict[food_item.id] = food_item  # force it on the overflow
+
+        now_on_frozen = shelves['frozen'].food_dict.get(food_item.id, None) is not None
+        self.assertEqual(now_on_frozen, False)
+        now_on_standby = shelves['overflow'].food_dict.get(food_item.id, None) is not None
+        self.assertEqual(now_on_standby, True)
+
+        shelves['frozen'].food_dict = dict()  # destroy the old dict
+        restore_to_proper_shelf(shelves)  # this should move it to the frozen shelf
+
+        now_on_frozen = shelves['frozen'].food_dict.get(food_item.id, None) is not None
+        self.assertEqual(now_on_frozen, True)
+        now_on_standby = shelves['overflow'].food_dict.get(food_item.id, None) is not None
+        self.assertEqual(now_on_standby, False)
+
 
 if __name__ == '__main__':
     unittest.main()
